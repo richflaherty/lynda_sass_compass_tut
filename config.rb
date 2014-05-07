b2fcd5dc1c3c47a8ce09bc0e95e860d2e5cafea9
richflaherty <rich_flaherty@yahoo.com>
2014-05-07 17:22:04 -0400

    Upgraded susy and added Breakpoint
    susy 2.1.2 and breakpoint 2.4.2 (Compass 1.0.0.alpha.19 and Sass 3.3.6)

--- config.rb
+++ config.rb
@@ -1,6 +1,7 @@
 require "susy"
+require 'breakpoint'
 css_dir = '_/css'
 sass_dir = '_/components/sass'
 javascripts_dir = '_/js'
 output_style = :compressed
-relative_assets = true;
+relative_assets = true
